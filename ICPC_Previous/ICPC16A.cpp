27220b9e4ac9d92d3592c8f153b6b5d333d33e45
Ashish Shevale <shevaleashish@gmail.com>
2021-01-25 19:26:08 +0530

    ICPC16A

---
+++ ICPC_Previous/ICPC16A.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	
+	while( t-- )
+	{
+    	int x1, y1, x2, y2;
+    	cin >> x1 >> y1 >> x2 >> y2;
+	
+	    if( x1 == x2 )
+    	{
+	        if( y1 < y2 )
+	            cout << "up\n";
+	        else
+    	        cout << "down\n";
+	    }
+    	else if( y1 == y2 )
+	    {
+	        if( x1 < x2 )
+	            cout << "right\n";
+    	    else
+	            cout << "left\n";
+	    }
+	    else
+	        cout << "sad\n";
+	}
+	
+	return 0;
+}
+

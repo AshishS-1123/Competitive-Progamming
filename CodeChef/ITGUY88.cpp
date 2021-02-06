73278b96991fe9c3666003032b06131e8c73a555
Ashish Shevale <shevaleashish@gmail.com>
2021-02-06 11:14:32 +0530

    ITGUY88

---
+++ CodeChef/ITGUY88.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false); 
+    cin.tie(NULL);
+    
+    int t;
+    cin >> t;
+    
+    while( t-- )
+    {
+        int k;
+        cin >> k;
+        
+        int num = 1;
+        
+        for(int i = 0; i < k; ++i)
+        {
+            for(int j = 0; j < i+1; ++j)
+            {
+                cout << num << "*";
+                ++num;
+            }
+            cout << "\n";
+        }
+    }
+    
+	return 0;
+}
+
